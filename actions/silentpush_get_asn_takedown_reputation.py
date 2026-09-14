bf4fa26c06432a9b2c4d9e95a07ca274439cf9cd
Jacob Davis <jacobd@splunk.com>
2026-09-14 10:39:11 -0700

    fix: encode Silent Push URL path parameters
    PSAAS-31333: Encode caller-controlled URL path segments before endpoint substitution, including optional live-WHOIS and IP resource paths.
    
    Authored by Codex.

--- actions/silentpush_get_asn_takedown_reputation.py
+++ actions/silentpush_get_asn_takedown_reputation.py
@@ -13,7 +13,7 @@
 # either express or implied. See the License for the specific language governing permissions
 # and limitations under the License.
 
-from urllib.parse import urlencode
+from urllib.parse import quote, urlencode
 
 import phantom.app as phantom
 
@@ -87,7 +87,7 @@ class GetAsnTakedownReputation(BaseAction):
 
         endpoint = consts.GET_ASN_TAKEDOWN_REPUTATION_ENDPOINT
         for parameter in parameters:
-            endpoint = endpoint.replace("{{##}}".replace("##", parameter), str(self._param.get(parameter)))
+            endpoint = endpoint.replace("{{##}}".replace("##", parameter), quote(str(self._param.get(parameter)), safe=""))
 
         return endpoint, "get"
 
--- actions/silentpush_get_subnet_reputation.py
+++ actions/silentpush_get_subnet_reputation.py
@@ -13,7 +13,7 @@
 # either express or implied. See the License for the specific language governing permissions
 # and limitations under the License.
 
-from urllib.parse import urlencode
+from urllib.parse import quote, urlencode
 
 import phantom.app as phantom
 
@@ -80,7 +80,7 @@ class GetSubnetReputation(BaseAction):
 
         endpoint = consts.GET_SUBNET_REPUTATION_ENDPOINT
         for parameter in parameters:
-            endpoint = endpoint.replace("{{##}}".replace("##", parameter), str(self._param.get(parameter)))
+            endpoint = endpoint.replace("{{##}}".replace("##", parameter), quote(str(self._param.get(parameter)), safe=""))
 
         return endpoint, "get"
 
